bf7a19a5a7cecf76ea42a310b30b0e189ba94bf8
Alina Artemiuk <alinaaartemiuk@gmail.com>
2024-02-07 14:33:58 +0100

    pep8

--- problem_set2/mountain.py
+++ problem_set2/mountain.py
@@ -2,7 +2,7 @@ import numpy as np
 from typing import Tuple
 
 
-class Mountain():
+class Mountain:
     def __init__(self):
         self.actions = ["upforward", "forward", "downforward"]
         self.actions_to_dir = {"upforward": (-1, 1), "forward": (0, 1),
@@ -25,9 +25,9 @@ class Mountain():
 
     def _check_state(self, state: Tuple[int, int]):
         i, j = state
-        if (i < 0 or i > 30):
+        if i < 0 or i > 30:
             raise ValueError(f'Row position, {i} is beyond the gridworld')
-        if (j < 0 or j > 99):
+        if j < 0 or j > 99:
             raise ValueError(f'Column position, {j}  is beyond the gridworld')
 
     def _check_action(self, action: str):
@@ -39,29 +39,29 @@ class Mountain():
         self._check_action(action)
 
         if action == "forward":
-            if (state[1] <= 98):
-                return (state[0], state[1] + 1)
+            if state[1] <= 98:
+                return state[0], state[1] + 1
             else:
                 return state
         if action == "upforward":
-            if (state[0] > 0 and state[1] <= 98):
-                return (state[0] - 1, state[1] + 1)
-            elif (state[0] == 0 and state[1] <= 98):
-                return (state[0], state[1] + 1)
+            if state[0] > 0 and state[1] <= 98:
+                return state[0] - 1, state[1] + 1
+            elif state[0] == 0 and state[1] <= 98:
+                return state[0], state[1] + 1
             else:
                 return state
         if action == "downforward":
-            if (state[0] < 30 and state[1] <= 98):
-                return (state[0] + 1, state[1] + 1)
-            elif (state[0] == 30 and state[1] <= 98):
-                return (state[0], state[1] + 1)
+            if state[0] < 30 and state[1] <= 98:
+                return state[0] + 1, state[1] + 1
+            elif state[0] == 30 and state[1] <= 98:
+                return state[0], state[1] + 1
             else:
                 return state
 
     def get_time(self, state: Tuple[int, int]) -> float:
         """
-        We want to minimize accumulated time getting accross the terrain. The time it takes the sled to cover one unit of distance on the hill depends directly on how rough
-        roughness is equivalent to the time.  
+        We want to minimize accumulated time getting across the terrain. The time it takes the sled to cover one unit
+        of distance on the hill depends directly on how rough roughness is equivalent to the time.
 
         input: 
             state: position (i,j) or equivalently (y,x)
@@ -69,15 +69,16 @@ class Mountain():
         """
         self._check_state(state)
         i, j = state
-        if (j <= 98):
+        if j <= 98:
             return self.map[i, j]
         else:
             return 0
 
     def get_reward(self, state: Tuple[int, int], action: str) -> float:
         """
-        We want to minimize accumulated time  getting accross the terrain. The time it takes the sled to cover one unit of distance on the hill depends directly on how rough
-        roughness is equivalent to the time. This is equivalent to maximizing the accumulated negative time
+        We want to minimize accumulated time  getting accross the terrain. The time it takes the sled to cover one unit
+        of distance on the hill depends directly on how rough roughness is equivalent to the time. This is equivalent to
+        maximizing the accumulated negative time
 
         input: 
             state: position (i,j) or equivalently (y,x)
