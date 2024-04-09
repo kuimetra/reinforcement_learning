c1a2f58bb9202b94e1b79b9fd63cd3d02df07391
Alina Artemiuk <alinaaartemiuk@gmail.com>
2024-04-09 16:48:12 +0200

    add .gitignore

---
+++ final_project/cookiedisaster/__init__.py
@@ -0,0 +1,23 @@
+from gymnasium.envs.registration import register
+
+config_v1 = {"img": "cookie", "width": 10, "lifetime": 5, "friction": lambda vel: -(vel ** 2) * 0.05}
+config_v2 = {"img": "cake", "width": 5, "lifetime": 4, "friction": lambda vel: -(vel ** 2) * 0.08}
+config_v3 = {"img": "scones", "width": 12, "lifetime": 7, "friction": lambda vel: -(vel ** 2) * 0.1}
+
+register(
+    id='cookiedisaster-v1',
+    entry_point="cookiedisaster.envs:CookieDisasterEnv",
+    kwargs={'config': config_v1}
+)
+
+register(
+    id='cookiedisaster-v2',
+    entry_point="cookiedisaster.envs:CookieDisasterEnv",
+    kwargs={'config': config_v2}
+)
+
+register(
+    id='cookiedisaster-v3',
+    entry_point="cookiedisaster.envs:CookieDisasterEnv",
+    kwargs={'config': config_v3}
+)
